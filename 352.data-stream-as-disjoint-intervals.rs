1e87129bbdfe55a50cbfa8fc0c826e1ad7231e73
Kevan Hollbach <khollbach@gmail.com>
2023-07-31 21:50:43 -0700

    352

--- 352.data-stream-as-disjoint-intervals.rs
+++ 352.data-stream-as-disjoint-intervals.rs
@@ -5,35 +5,40 @@
  */
 
 // @lc code=start
+
+/// Upper limit, exclusive.
+const N: usize = 10usize.pow(4) + 1;
+
 struct SummaryRanges {
-
+    seen: Box<[bool; N]>,
 }
 
-
-/** 
- * `&self` means the method takes an immutable reference.
- * If you need a mutable reference, change it to `&mut self` instead.
- */
 impl SummaryRanges {
-
     fn new() -> Self {
-        
+        Self {
+            seen: Box::new([false; N]),
+        }
     }
-    
-    fn add_num(&self, value: i32) {
-        
+
+    fn add_num(&mut self, value: i32) {
+        assert!(0 <= value && value < N as i32);
+        self.seen[value as usize] = true;
     }
-    
+
     fn get_intervals(&self) -> Vec<Vec<i32>> {
-        
+        let mut out = vec![];
+
+        let mut indices = 0..N;
+
+        while let Some(start) = indices.find(|&i| self.seen[i]) {
+            let end = indices.find(|&i| !self.seen[i]).unwrap_or(N);
+
+            let end_inclusive = end - 1;
+            out.push(vec![start as i32, end_inclusive as i32]);
+        }
+
+        out
     }
 }
 
-/**
- * Your SummaryRanges object will be instantiated and called as such:
- * let obj = SummaryRanges::new();
- * obj.add_num(value);
- * let ret_2: Vec<Vec<i32>> = obj.get_intervals();
- */
 // @lc code=end
-
