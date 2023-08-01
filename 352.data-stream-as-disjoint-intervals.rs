1f29a5abd1ecc9a519102cf51c793599d7ce6ae8
Kevan Hollbach <khollbach@gmail.com>
2023-07-31 21:31:04 -0700

    Initial commit

---
+++ 352.data-stream-as-disjoint-intervals.rs
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode id=352 lang=rust
+ *
+ * [352] Data Stream as Disjoint Intervals
+ */
+
+// @lc code=start
+struct SummaryRanges {
+
+}
+
+
+/** 
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl SummaryRanges {
+
+    fn new() -> Self {
+        
+    }
+    
+    fn add_num(&self, value: i32) {
+        
+    }
+    
+    fn get_intervals(&self) -> Vec<Vec<i32>> {
+        
+    }
+}
+
+/**
+ * Your SummaryRanges object will be instantiated and called as such:
+ * let obj = SummaryRanges::new();
+ * obj.add_num(value);
+ * let ret_2: Vec<Vec<i32>> = obj.get_intervals();
+ */
+// @lc code=end
+
